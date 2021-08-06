b80b82de9c519ec8f3bbd5704f009ee43a170cb1
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-06 22:43:34 +0200

    atualizando php

--- php/insertall.php
+++ php/insertall.php
@@ -1,45 +1,37 @@
-<?php
-include('header.php');
-
-$nome=$_POST['nome'];
-$email=$_POST['email'];
-$pass=$_POST['pass'];
-$provincia=$_POST['provincia'];
-$municipio=  $_POST['municipio'];
-$tel=$_POST['tel'];
-$hobbies=$_POST['habilidades'];
-$sql="INSERT INTO cadastrados (nome,email,pass,provincia,municipio,tel,habilidades, momento) VALUES('$nome','$email','$pass','$provincia','$municipio','$tel','$hobbies', now())";
-$insert=$mysqli_connection->query($sql);
-if($insert){
-?>
-  
-  
-  
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Criar conta</title>
-  
-  <script src="materialize/js/materialize.min.js"></script>
-    <link rel="icon" type="img/blogs-for-freelancers.png" href="img/blogs-for-freelancers.png"/>
-
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="materialize/css/materialize.css">
-
-<link rel="stylesheet" href="materialize/css/materialize.min.css">
-
+  <link rel="icon" href="img/blogs-for-freelancers.png" />
+  <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="css/materialize/materialize.min.css">
 </head>
+
 <body>
   <?php
-    echo"Sua conta foi criada, <a href='iniciarsessao.html'> inicie sessão</a>";
-}
-else{
-    echo"Ocorreu um Erro ao criar sua conta !  <a href='CriarConta.html'>  tente novamente</a> .";
-}
+  include('header.php');
 
+  $nome = $_POST['nome'];
+  $email = $_POST['email'];
+  $pass = $_POST['pass'];
+  $provincia = $_POST['provincia'];
+  $municipio =  $_POST['municipio'];
+  $tel = $_POST['tel'];
+  $hobbies = $_POST['habilidades'];
+
+  $sql = "INSERT INTO cadastrados (nome,email,pass,provincia,municipio,tel,habilidades, momento) VALUES('$nome','$email','$pass','$provincia','$municipio','$tel','$hobbies', now())";
+  $insert = $mysqli_connection->query($sql);
+
+  if ($insert) {
+    echo "Sua conta foi criada, <a href='iniciarsessao.html'>inicie sessão para continuar..</a>";
+  } else {
+    echo "Ocorreu um Erro ao criar sua conta, <a href='CriarConta.html'>tente novamente</a> .";
+  }
   ?>
 </body>
+
 </html>

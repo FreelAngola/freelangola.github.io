789828683aaf6ef83ddeb2f26e144136674eb0e6
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-06 23:15:21 +0200

    atualizando php

--- php/msgs.php
+++ php/msgs.php
@@ -1,22 +1,19 @@
 <?php
 session_start();
-include_once("header.php"); 
-?>
-<?php
+include_once("header.php");
 
-$username=$_SESSION['login'];
-$id=$_SESSION['id'];
-$userid=$_SESSION['id'];
-$user=$_POST['code'];
-$msg=$_POST['body'];
-$code=$id+$user;
+$username = $_SESSION['login'];
+$id = $_SESSION['id'];
+$userid = $_SESSION['id'];
+$user = $_POST['code'];
+$msg = $_POST['body'];
+$code = $id + $user;
 
-if(empty($msg)){
-echo"<h1>por favor escreva alguma coisa!</h1>";}
+if (empty($msg)) {
+   echo "<h1>por favor escreva alguma coisa!</h1>";
+} else {
+   $insere = "INSERT INTO msg (mensageiro,mensagem,codigo,sessao, momento) Values ('$username','$msg','$code','$userid',now())";
+   $resultado = $mysqli_connection->query($insere);
 
-else{
-$insere = "INSERT INTO msg (mensageiro,mensagem,codigo,sessao, momento) Values ('$username','$msg','$code','$userid',now())";  
-   $resultado = $mysqli_connection->query($insere); 
-
-header("Location:mensagem.php?iduser=$user");}
-?>
+   header("Location:mensagem.php?iduser=$user");
+}

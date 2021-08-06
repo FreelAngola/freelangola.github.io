0003f7a43184f7341d7bc15f582b2c81385afba5
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-06 22:43:19 +0200

    atualizando php

--- php/gostar.php
+++ php/gostar.php
@@ -1,13 +1,11 @@
-<?php
-session_start();
-include_once('header.php');
-   ?>
-   <?php
-$poster= $_SESSION['login'];
-$post= $_GET['postnumb'];
-
-$sql="INSERT INTO gostos (liker, likes, post) VALUES('$poster',' 1 ','$post')";
-$insert=mysqli_query($mysqli_connection,$sql);
-
-header('location:painel.php')
-?>
+<?php
+session_start();
+include_once('header.php');
+
+$poster = $_SESSION['login'];
+$post = $_GET['postnumb'];
+
+$sql = "INSERT INTO gostos (liker, likes, post) VALUES('$poster',' 1 ','$post')";
+$insert = mysqli_query($mysqli_connection, $sql);
+
+header('location:painel.php');

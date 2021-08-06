3b7f23130ae42c84973b12a1ab2594754fb23d4e
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-06 20:49:04 +0200

    atualizando php

--- php/add_post_from_perfil.php
+++ php/add_post_from_perfil.php
@@ -1,18 +1,10 @@
 <?php 
-
 session_start();
-
 include_once("header.php"); 
 
 $username = $_SESSION['nome']; 
-
 $body=$_POST['body'];
-
 $insere = "INSERT into postagens (username, conteudo, momento) Values ('$username', '$body', now())";  
-
 $result = mysqli_query($conexao, $insere); 
 
-header("Location:perfil.php"); 
-
-?>
-
+header("Location:perfil.php");

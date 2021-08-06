fcd1e22707ce107fb648db99627974998cbcbea2
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-06 22:43:39 +0200

    atualizando php

--- php/like.php
+++ php/like.php
@@ -1,13 +1,12 @@
-<?php
-session_start();
-include("header.php");
-?>
-<?php
-$post=$_GET['post'];
-$id=$_SESSION['id'];
- $inserira =  "INSERT INTO likes (post_id,likes,liker) VALUES ('$post',1,'$id')";
- $resultadoa = $mysqli_connection->query($inserira);
- 
- header("location:site.php");
-
-?>
+<?php
+session_start();
+include("header.php");
+
+
+$post = $_GET['post'];
+$id = $_SESSION['id'];
+
+$inserir =  "INSERT INTO likes (post_id,likes,liker) VALUES ('$post',1,'$id')";
+$resultados = $mysqli_connection->query($inserir);
+
+header("location:site.php");

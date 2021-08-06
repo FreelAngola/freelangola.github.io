29fda3511b7a5427f8a1cfbf33fc1d00aecd9881
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-06 20:49:28 +0200

    atualizando php

--- php/citacao.php
+++ php/citacao.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>postar uma citação</title>
-    </head>
-    <body>
-        <form action="site.php" method="post">
-            <textarea name="post" cols="30" rows="4" placeholder="escreva aqui a citação"></textarea>
-            <div class="div_sub">
-                <input  type="submit" value="postar">
-            </div>
-        </form>
-    </body>
+<html lang="pt">
+
+<head>
+    <title>postar uma citação</title>
+</head>
+
+<body>
+    <form action="site.php" method="post">
+        <textarea name="post" cols="30" rows="4" placeholder="escreva aqui a citação"></textarea>
+        <div class="div_sub">
+            <input type="submit" value="postar">
+        </div>
+    </form>
+</body>
+
 </html>

dfef801d17862574c685a931b29c232179bc6ea9
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-06 20:49:08 +0200

    atualizando php

--- php/add.php
+++ php/add.php
@@ -3,10 +3,11 @@ include_once("site.php");
 
 $username = $log;
 $body=$_POST['body'];
-if(!empty($body)){$insere = "INSERT INTO postagens (username,conteudo,momento) Values ('$username','$body',now())";  
-   $resultado = $mysqli_connection->query($insere); }
+
+if(!empty($body)){
+   $insere = "INSERT INTO postagens (username,conteudo,momento) Values ('$username','$body',now())";  
+   $resultado = $mysqli_connection->query($insere);
+}
 else{
    header('location:perfil.php');
 }
-
-?>

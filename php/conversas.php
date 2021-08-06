d04c7ffccecf97708d2396acc468d0c68851fd3e
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-06 20:49:48 +0200

    atualizando php

--- php/conversas.php
+++ php/conversas.php
@@ -1,19 +1,20 @@
 <?php
-    session_start();
-    include_once('header.php');
+session_start();
+include_once('header.php');
 
-    $local = '';
-    $room = 'd-none';
+$local = '';
+$room = 'd-none';
 
-    if (isset($_GET['room_with']) and $_GET['room_with'] != '') {
-        $local = 'd-none';
-        $room = '';
-        $chat_with = $_GET['room_with'];
-    }
+if (isset($_GET['room_with']) and $_GET['room_with'] != '') {
+    $local = 'd-none';
+    $room = '';
+    $chat_with = $_GET['room_with'];
+}
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt">
+
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -21,6 +22,7 @@
     <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="../css/style.css">
 </head>
+
 <body>
     <main class="main" role="main">
         <nav class="navbar navbar-expand-md navbar-light bg-white box-shadow">
@@ -40,23 +42,23 @@
             </div>
         </nav>
         <?php
-            $eu=$_SESSION['id'];
-            $conPoste = "SELECT DISTINCT eu FROM msg WHERE seu_id='$eu'"; 
-            $resulte =  mysqli_query($mysqli_connection, $conPoste); 
-            while($dadose = mysqli_fetch_array($resulte)){  
-                $msgs= $dadose['eu'];
-                if($msgs==$eu){
-                    echo "";
-                }
-                else{
-                    $consult = "SELECT id FROM cadastrados where nome='$msgs'"; 
-                    $resultad = mysqli_query($mysqli_connection, $consult ); 
-                    $dad = mysqli_fetch_array($resultad); 
-                    $id= $dad['id'];
-                    echo "<a href='mensagens.php?usernumer=$id'>".$msgs."</a><hr>";
-                }
+        $eu = $_SESSION['id'];
+        $conPoste = "SELECT DISTINCT eu FROM msg WHERE seu_id='$eu'";
+        $resulte =  mysqli_query($mysqli_connection, $conPoste);
+        while ($dadose = mysqli_fetch_array($resulte)) {
+            $msgs = $dadose['eu'];
+            if ($msgs == $eu) {
+                echo "";
+            } else {
+                $consult = "SELECT id FROM cadastrados where nome='$msgs'";
+                $resultad = mysqli_query($mysqli_connection, $consult);
+                $dad = mysqli_fetch_array($resultad);
+                $id = $dad['id'];
+                echo "<a href='mensagens.php?usernumer=$id'>" . $msgs . "</a><hr>";
             }
+        }
         ?>
     </main>
 </body>
+
 </html>

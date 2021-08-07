8aab9bef83b3e3830373beb31cd0d89de645a8e0
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-07 18:42:58 +0200

    atualizando php

--- php/post.php
+++ php/post.php
@@ -1,36 +1,35 @@
-<?php
-session_start();
-include_once('header.php');
-   ?>
-   <?php
-
-$poster= $_SESSION['login'];
-$id= $_SESSION['id'];
-$post= $_POST['mensagem'];
-
-if($post==""){echo "
-    <!DOCTYPE html>
-    <html lang='en'>
-    <head>
-        <meta charset='UTF-8'>
-        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-        <title>Document</title>
-    </head>
-    <body>
-    <script>
-      window.alert('Por favor escreva algo no formulário!')
-
-    </script>
-    </body>
-    </html>";
-    
-header('location:painel.php');
-}
-else{
-$sql="INSERT INTO postagens (poster, poster_id, post, momento) VALUES('$poster','$id','$post', now())";
-$insert=mysqli_query($mysqli_connection,$sql);
-
-header('location:painel.php');
-}
-?>
+<?php
+
+session_start();
+
+include_once('header.php');
+
+$poster = $_SESSION['login'];
+$id = $_SESSION['id'];
+$post = $_POST['mensagem'];
+
+if ($post == "") {
+  echo "
+    <!DOCTYPE html>
+    <html lang='pt'>
+
+    <head>
+        <meta charset='UTF-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>Document</title>
+    </head>
+
+    <body>
+    <script>
+      window.alert('Por favor escreva algo no formulário!')
+    </script>
+    </body>
+    </html>";
+
+  header('location:painel.php');
+} else {
+  $sql = "INSERT INTO postagens (poster, poster_id, post, momento) VALUES('$poster','$id','$post', now())";
+  $insert = mysqli_query($mysqli_connection, $sql);
+  header('location:painel.php');
+}

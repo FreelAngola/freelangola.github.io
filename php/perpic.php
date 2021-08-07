ea1d531c6f69ad742f5ef1c134355ee47141e0d6
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-07 18:42:45 +0200

    atualizando php

--- php/perpic.php
+++ php/perpic.php
@@ -1,24 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adiciocar foto de perfil</title>
+    <style>
+        h1 {
+            color: violet;
+        }
+
+        #i {
+            width: 100%;
+            margin-top: 4px;
+        }
+    </style>
 </head>
-<style>
-    h1{
-        color:violet;
-    }
-    #i{
-        width:100%;
-        margin-top:4px;
-    }
-</style>
+
 <body>
-    <p><h1>escolha uma foto</h1></p>
+    <p>
+    <h1>escolha uma foto</h1>
+    </p>
     <form action="per.php" method="post" enctype="multipart/form-data">
-   <input type="file" name="file" id=""><br>
-   <input type="submit" name="submit" value="carregar" id="i">
-</form>
+        <input type="file" name="file" id=""><br>
+        <input type="submit" name="submit" value="carregar" id="i">
+    </form>
 </body>
+
 </html>

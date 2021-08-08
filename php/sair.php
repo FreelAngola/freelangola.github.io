7c85d6a33ac98d4367aa2a2df85821f5899b7182
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-08 02:44:18 +0200

    atualizando php

--- php/sair.php
+++ php/sair.php
@@ -1,13 +1,7 @@
 <?php
 session_start();
 include_once("header.php");
-?>
-
-<?php
 
 $online2 =  "UPDATE cadastrados SET online = '0' WHERE pp = '".$_SESSION['pp']."' AND telefone= '".$_SESSION['telefone']."'";
 $set=$mysqli_connection->query($online2);
 session_destroy();
-
-
-?>
